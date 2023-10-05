389446c0a1bb3159c5e5c107ff85836edb0bbf70
OlyFern <farringtonchandler@gmail.com>
2023-10-04 20:01:09 -0700

    Followed tutorial

--- src/main/java/org/example/StreamAPITutorial.java
+++ src/main/java/org/example/StreamAPITutorial.java
@@ -2,6 +2,9 @@ package org.example;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamAPITutorial {
 
@@ -17,10 +20,150 @@ public class StreamAPITutorial {
                 .sorted()
                 .forEach(System.out::println);
 
+
         // C1
         // C2
 
-        // TODO add more tutorial code here
+       // Making an array, turning it into a parallel stream, then prints out the first element if it is there
+        Arrays.asList("a1", "a2", "a3")
+                .stream()
+                .findFirst()
+                .ifPresent(System.out::println);  // a1
+
+
+        // Makes a stream directly, then prints out the first element if it is there
+        Stream.of("a1", "a2", "a3")
+                .findFirst()
+                .ifPresent(System.out::println);  // a1
+
+        // Makes a stream of int with a range of 1 to 4 (does not include 4) then prints each element
+        IntStream.range(1, 4)
+                .forEach(System.out::println);
+
+        /* this was a test by me
+        IntStream.range(1,4)
+                .map(n -> 2 * n + 1)
+                .average()
+                .ifPresent(System.out::println);
+        */
+
+        Arrays.stream(new int[] {1, 2, 3})
+                .map(n -> 2 * n + 1) //takes each number in the stream and runs this fuctiuon through it
+                .average() //takes the average of the whole stream
+                .ifPresent(System.out::println);  // 5.0
+
+        Stream.of("a1", "a2", "a3")
+                .map(s -> s.substring(1))
+                .mapToInt(Integer::parseInt)
+                .max()
+                .ifPresent(System.out::println);  // 3
+
+        IntStream.range(1, 4)
+                .mapToObj(i -> "a" + i)
+                .forEach(System.out::println);
+
+
+        Stream.of(1.0, 2.0, 3.0)
+                .mapToInt(Double::intValue)
+                .mapToObj(i -> "a" + i)
+                .forEach(System.out::println);
+
+
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .filter(s -> {
+                    System.out.println("filter: " + s);
+                    return true;
+                });
+
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .filter(s -> {
+                    // this next line will only print if there is another action after it. think of this whole section as one connected line of code
+                    // this is simply checking if the filter is true then move on to the next part, but if there is no next part it just moves on
+                    // if it returned false then the filter would print, but the forEach would not
+                     System.out.println("filter: " + s);
+                    return true;
+                }).forEach(s -> System.out.println("forEach: " + s));
+
+
+
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toUpperCase();
+                })
+                .anyMatch(s -> { // stops checking if starts with A after it found 1
+                    System.out.println("anyMatch: " + s);
+                    return s.startsWith("A");
+                });
+
+
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toUpperCase();
+                })
+                .filter(s -> {
+                    System.out.println("filter: " + s);
+                    return s.startsWith("A");
+                })
+                .forEach(s -> System.out.println("forEach: " + s)); //will only print forEach if filter returns true
+
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .filter(s -> {
+                    System.out.println("filter: " + s);
+                    return s.startsWith("a");
+                }) // will only go through with rest of code if filter returns true
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toUpperCase();
+                })
+                .forEach(s -> System.out.println("forEach: " + s));
+
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .sorted((s1, s2) -> {
+                    System.out.printf("sort: %s; %s\n", s1, s2);
+                    return s1.compareTo(s2);
+                })
+                .filter(s -> {
+                    System.out.println("filter: " + s);
+                    return s.startsWith("a");
+                })
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toUpperCase();
+                })
+                .forEach(s -> System.out.println("forEach: " + s));
+
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .filter(s -> {
+                    System.out.println("filter: " + s);
+                    return s.startsWith("a");
+                })
+                .sorted((s1, s2) -> {
+                    System.out.printf("sort: %s; %s\n", s1, s2);
+                    return s1.compareTo(s2);
+                })
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toUpperCase();
+                })
+                .forEach(s -> System.out.println("forEach: " + s));
+
+        Stream<String> stream =
+                Stream.of("d2", "a2", "b1", "b3", "c")
+                        .filter(s -> s.startsWith("a"));
+
+        stream.anyMatch(s -> true);    // ok
+        //stream.noneMatch(s -> true);   // exception
+
+        Supplier<Stream<String>> streamSupplier =
+                () -> Stream.of("d2", "a2", "b1", "b3", "c")
+                        .filter(s -> s.startsWith("a"));
+
+        streamSupplier.get().anyMatch(s -> true);   // ok
+        streamSupplier.get().noneMatch(s -> true);  // ok
+
+
 
     }
 }
